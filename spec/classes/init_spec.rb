1af065bc66a582471d03d4b9d250dd29578a3e44
Garrett Honeycutt <code@garretthoneycutt.com>
2013-12-10 15:18:29 +0800

    Validate templates and ruby files
    This patch adds validation for ruby files and templates while also
    providing an extremely minimal set of spec tests.

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+describe 'rancid' do
+
+  context 'with default params on EL 6' do
+    let(:facts) do
+      { :osfamily          => 'RedHat',
+        :lsbmajdistrelease => '6',
+      }
+    end
+
+    it { should compile.with_all_deps }
+  end
+
+  context 'with default params on osfamily Debian' do
+    let(:facts) { { :osfamily => 'Debian' } }
+
+    it { should compile.with_all_deps }
+  end
+end
