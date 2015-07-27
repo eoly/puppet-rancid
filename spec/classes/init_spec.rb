a1991fd53b82bde6edfddc9d2d26b689904f957f
Mason Donahue <mdonahue@backstopsolutions.com>
2015-07-27 16:00:51 -0500

    Add show_cloginrc_diff param to allow hiding passwords
    $show_cloginrc_diff now allows for the disabling of the 'show_diff' flag
    on the cloginrc file, for users that store their router passwords in
    that file and do not want them logged on puppet runs.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,11 +9,40 @@ describe 'rancid' do
     end
 
     it { should compile.with_all_deps }
+
+    context 'with show_cloginrc_diff unset' do
+      it {should contain_file('rancid_cloginrc').with_show_diff('true')}
+    end
+
+    context 'with show_cloginrc_diff set to true' do
+      let(:params) { {:show_cloginrc_diff => true} }
+      it {should contain_file('rancid_cloginrc').with_show_diff('true')}
+    end
+
+    context 'with show_cloginrc_diff set to false' do
+      let(:params) { {:show_cloginrc_diff => false} }
+      it {should contain_file('rancid_cloginrc').with_show_diff('false')}
+    end
   end
 
   context 'with default params on osfamily Debian' do
     let(:facts) { { :osfamily => 'Debian' } }
 
     it { should compile.with_all_deps }
+
+    context 'with show_cloginrc_diff unset' do
+      it {should contain_file('rancid_cloginrc').with_show_diff('true')}
+    end
+
+    context 'with show_cloginrc_diff set to true' do
+      let(:params) { {:show_cloginrc_diff => true} }
+      it {should contain_file('rancid_cloginrc').with_show_diff('true')}
+    end
+
+    context 'with show_cloginrc_diff set to false' do
+      let(:params) { {:show_cloginrc_diff => false} }
+      it {should contain_file('rancid_cloginrc').with_show_diff('false')}
+    end
   end
+
 end
