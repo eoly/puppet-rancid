57059a1db3fbe6bf308bf4a9e34082fc3bfdf0a1
Neil Hemingway <neil.hemingway@greyhavens.org.uk>
2021-08-12 10:15:09 +0100

    Manage git remote for device groups
    Closes #27

--- manifests/router_db.pp
+++ manifests/router_db.pp
@@ -1,9 +1,10 @@
 # == Define: rancid::router_db
 #
 define rancid::router_db (
-  $devices         = undef,
-  $rancid_cvs_path = '/bin:/usr/bin',
-  $router_db_mode  = '0640',
+  Hash $devices         = {},
+  $rancid_cvs_path      = '/bin:/usr/bin',
+  $router_db_mode       = '0640',
+  Hash $vcs_remote_urls = {},
 ) {
   include rancid
 
@@ -12,6 +13,32 @@ define rancid::router_db (
     path    => $rancid_cvs_path,
     user    => $rancid::user_real,
     unless  => "test -d ${rancid::homedir_real}/${name}/CVS",
+  }
+
+  if ( $vcs_remote_urls[$name] ) {
+    $remote_url = $vcs_remote_urls[$name]
+    exec { "setup git remote ${name}":
+      command => "git remote set-url origin ${remote_url}",
+      cwd     => "${rancid::homedir_real}/${name}",
+      path    => $rancid_cvs_path,
+      user    => $rancid::user_real,
+      unless  => "git remote -v | grep ${remote_url}"
+    }
+
+    file { "post-commit hook for ${name}":
+      path    => "${rancid::homedir_real}/${name}/.git/hooks/post-commit",
+      content => "git push origin\n",
+      owner   => $rancid::user_real,
+      group   => $rancid::group_real,
+      mode    => '0755',
+    }
+
+    file { "rancid default git remote ${name}":
+      path   => "${rancid::homedir_real}/.git/${name}",
+      ensure => absent,
+      force  => true,
+      backup => false,
+    }
   }
 
   if ( $devices[$name] ) {
--- spec/defines/router_db_spec.rb
+++ spec/defines/router_db_spec.rb
@@ -56,4 +56,39 @@ describe 'rancid::router_db' do
                .with_mode(0123)
     }
   end
+
+  context 'managing remote urls' do
+    context 'we are managing them' do
+      let(:params) {
+        { :vcs_remote_urls => {
+            'group1' => 'http://my-server/my-path.git',
+          },
+        }
+      }
+
+      it 'should point at the requested remote url' do
+        should contain_exec('setup git remote group1')
+                 .with_command('git remote set-url origin http://my-server/my-path.git')
+                 .with_cwd('/var/lib/rancid/group1')
+      end
+
+      it 'should deploy a post commit hook to auto-push to the remote' do
+        should contain_file('post-commit hook for group1')
+      end
+
+      it 'should remove the default (local) remote' do
+        should contain_file('rancid default git remote group1')
+      end
+    end
+
+    context 'we are not managing them' do
+      let(:params) {
+        {}
+      }
+
+      it {
+        should_not contain_exec(/setup git remote/)
+      }
+    end
+  end
 end
